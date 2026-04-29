451ebdd63e1d45b9112185ada6a4bb36afd5f7e8
ndiayeismaila-prog <ndiayeismaila@djoloftech.com>
2026-04-29 18:26:19 +0200

    Add files via upload

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+function App() {
+      return (
+          <div style={{ padding: "20px", color: "white", background: "black" }}>
+                <h1>San Andreas History</h1>
+                      <p>Ton site fonctionne enfin 🔥</p>
+                          </div>
+                            );
+                            }
+
+                            export default App;
+}
